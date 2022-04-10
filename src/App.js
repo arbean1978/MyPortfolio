2d4d62358ab807fc383639bdbe45d063d1421a9f
Aaron Bean <arbean1978@gmail.com>
2022-04-10 09:50:12 -0600

    componentfolders

--- src/App.js
+++ src/App.js
@@ -15,3 +15,33 @@ function App() {
     "Contact",
     "Resume",
   ]);
+
+  const displaySection = () => {
+    if (currentSection === "About Me") {
+      console.log("current state: ", currentSection);
+      return <About />
+    } else if (currentSection === "Portfolio") {
+      console.log("current state: ", currentSection);
+      return <Portfolio />
+    } else if (currentSection === "Resume") {
+      console.log("current state: ", currentSection);
+      return <Resume />
+    } else if (currentSection === "Contact" ) {
+      console.log("current state: ", currentSection);
+      return <Contact />
+    } else {
+      console.log("current state: ", currentSection);
+      return <About />
+    }
+
+  }
+  return (
+    <main className="content">
+    <Header currentSection={currentSection} setCurrentSection={setCurrentSection} navSections={navSections} />
+        {displaySection()}
+    <Footer />
+    </main>
+  );
+}
+
+export default App;
